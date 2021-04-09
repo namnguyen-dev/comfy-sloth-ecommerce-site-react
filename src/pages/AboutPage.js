897b79f3aa9a9d70d61989fc652bc96a09512331
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-09 21:22:24 +0700

    update Pagehero, AboutPage, CheckoutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+            recusandae reiciendis quos incidunt facere ducimus sequi
+            perspiciatis, inventore voluptates veniam, eveniet, veritatis nam ex
+            laboriosam ut animi in. Quo, molestias?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;

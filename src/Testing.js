2924a257b9692025bb58b323da3ab7a78e6fbc13
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-09 18:33:19 +0700

    set up Router, pages

---
+++ src/Testing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const Testing = () => {
+    return (
+        <Wrapper>
+            <h3>hello world</h3>
+            <h3>hello world</h3>
+            
+        </Wrapper>
+    )
+}
+
+
+const Wrapper = styled.section`
+h3 {
+    color: magenta
+}
+`

2a6967e2502ec03e07281c2bf24e1b9632caf6ca
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-10 19:41:50 +0700

    update SingleProductPage

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading:false,
+  single_product_error:false,
+  single_product:{},
 };
 
 const ProductsContext = React.createContext();
@@ -45,9 +48,19 @@ export const ProductsProvider = ({ children }) => {
     } catch (error) {
       dispatch({type: GET_PRODUCTS_ERROR})
     }
-  
-   
   };
+
+  const fetchSingleProduct = async (url)=> {
+    dispatch({type:GET_SINGLE_PRODUCT_BEGIN})
+    try {
+      const response = await axios(url)
+      const singleProduct = response.data
+      dispatch({type: GET_SINGLE_PRODUCT_SUCCESS, payload:singleProduct})
+      
+    } catch (error) {
+      dispatch({type:GET_SINGLE_PRODUCT_ERROR})
+    }
+  }
 
   useEffect(() => {
     fetchProducts(`${url}`);
@@ -59,6 +72,7 @@ export const ProductsProvider = ({ children }) => {
         ...state,
         openSidebar,
         closeSidebar,
+        fetchSingleProduct,
       }}
     >
       {children}
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useProductsContext } from '../context/products_context';
+import { single_product_url as url } from '../utils/constants';
+import { formatPrice } from '../utils/helpers';
 import {
   Loading,
   Error,
@@ -10,13 +10,50 @@ import {
   AddToCart,
   Stars,
   PageHero,
-} from '../components'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+} from '../components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
-}
+  const { id } = useParams();
+  const history = useHistory();
+
+  const {
+    fetchSingleProduct,
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+  } = useProductsContext();
+  console.log(product);
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+  return (
+    <main>
+      <PageHero title={product.name} />
+      <Link to="/" className="btn">
+        back home
+      </Link>
+      <Wrapper></Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.main`
   .product-center {
@@ -50,6 +87,6 @@ const Wrapper = styled.main`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
-export default SingleProductPage
+export default SingleProductPage;

959de611bc61a251472d9f54441f85c56fea4eaa
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-10 15:40:38 +0700

    add Services section to Homepage

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { services } from '../utils/constants';
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper className="section">
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut magnam,
+            nisi aliquam quae numquam harum hic nihil accusantium porro
+            consequatur.
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service)=> {
+            const {id,title, text,icon}=service
+            return <article className="service">
+              <span className="icon">{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +94,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;

9014cf940bcac9b379143a8e2e23c2af4a54d4ec
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-10 15:50:03 +0700

    add contact section to Homepage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ut
+            numquam enim, eos tempora dolores quis maxime distinctio illum
+            temporibus.
+          </p>
+          <form className="contact-form">
+            <input type="text" className='form-input' placeholder='enter email'/>
+            <button type="submit" className='submit-btn'>subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +84,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;

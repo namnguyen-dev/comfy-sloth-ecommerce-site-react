1b97b4cc03108b05c1746285cd6f29b3967aa5d7
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-10 17:00:51 +0700

    add fetchProduct to product_context

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -22,13 +26,31 @@ const ProductsContext = React.createContext();
 export const ProductsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const openSidebar = () => {
+  const openSidebar = url => {
     dispatch({ type: SIDEBAR_OPEN });
   };
 
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async () => {
+    dispatch({type:GET_PRODUCTS_BEGIN})
+    try {
+      const response = await axios(url)
+      const products = response.data
+      dispatch({type:GET_PRODUCTS_SUCCESS,payload:products})
+      
+    } catch (error) {
+      dispatch({type:GET_PRODUCTS_ERROR})
+    }
+  
+   
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
     <ProductsContext.Provider

d033bfbb1d8b60536fa5c42bc26d37b0da551883
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-15 15:37:33 +0700

    uodate AddToCart reducer

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -11,6 +11,18 @@ const cart_reducer = (state, action) => {
     const { id, color, amount, product } = action.payload;
     const tempItem = state.cart.find(i => i.id === id + color);
     if (tempItem) {
+      const tempCart = state.cart.map(cartItem => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
     } else {
       const newItem = {
         id: id + color,
@@ -18,12 +30,11 @@ const cart_reducer = (state, action) => {
         color,
         amount,
         image: product.images[0].url,
-        price:product.price,
-        max:product.stock,
+        price: product.price,
+        max: product.stock,
       };
       return { ...state, cart: [...state.cart, newItem] };
     }
- 
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };

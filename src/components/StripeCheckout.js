1b1a2602807e40f1609796290a8125a8aad382c5
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-17 15:09:55 +0700

    update return, add new functionalities to CheckoutForm

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -47,8 +47,71 @@ const CheckoutForm = () => {
       },
     },
   };
+  const createPaymentIntent = async () => {};
 
-  return <h4>hello from Stripe Checkout </h4>;
+  const handleChange = async event => {
+    // Listen for changes in the CardElement
+    // and display any errors as the costomer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '');
+  };
+
+  const handleSubmit = async ev => {
+    ev.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
+  };
+
+  return (
+    <div>
+      <form id="payment-form" onSubmit={handleSubmit}>
+        <CardElement
+          id="card-element"
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button disabled={processing || disabled || succeeded}>
+          <span>
+            {processing ? (
+              <div className="spinner" id="spinner"></div>
+            ) : (
+              'Pay now'
+            )}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
+          Payment succeeded, see the result in your{' '}
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            {' '}
+            Stripe dashboard
+          </a>
+          <br />
+          Refresh the page to pay again
+        </p>
+      </form>
+    </div>
+  );
 };
 
 const StripeCheckout = () => {
